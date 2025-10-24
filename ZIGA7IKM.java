2ae1adba73a55882deb95b97bab742d26cf4e1b5
hoanhdungdz <124074080+hoanhdungdz@users.noreply.github.com>
2025-10-24 11:58:08 +0700

    Update ZIGA7IKM.java

--- ZIGA7IKM.java
+++ ZIGA7IKM.java
@@ -1,3 +1,9 @@
+// [Mã câu hỏi (qCode): ZIGA7IKM].  Một chương trình máy chủ cho phép kết nối qua TCP tại cổng 2207 (hỗ trợ thời gian liên lạc tối đa cho mỗi yêu cầu là 5s), yêu cầu xây dựng chương trình (tạm gọi là client) thực hiện kết nối tới server tại cổng 2207, sử dụng luồng byte dữ liệu (DataInputStream/DataOutputStream) để trao đổi thông tin theo thứ tự: 
+// a.	Gửi chuỗi là mã sinh viên và mã câu hỏi theo định dạng "studentCode;qCode". Ví dụ: "B15DCCN999;1D25ED92"
+// b.	Nhận lần lượt hai số nguyên a và b từ server
+// c.	Thực hiện tính toán tổng, tích và gửi lần lượt từng giá trị theo đúng thứ tự trên lên server
+// d.	Đóng kết nối và kết thúc
+
 package com.mycompany.ziga7ikm;
 
 import java.io.DataInputStream;
